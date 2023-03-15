50f875824d5af3c2d87258af827342f9143e1f07
ShakilJoy31 <joy31a@gmail.com>
2023-03-16 00:02:11 +0600

    changes done

--- pages/HomeComponent.js
+++ pages/HomeComponent.js
@@ -10,7 +10,7 @@ const HomeComponent = () => {
         <div>
             <div className="hero lg:mx-0 md:mx-0 mx-4">
                 <div className="hero-content flex-col lg:flex-row-reverse">
-                    <img src="https://khelo.live/wp-content/uploads/2020/07/MV5BM2U5ZTIxMzUtMmQ5MC00YTdhLThjOWYtODUzNDQ0MThmMmQ4XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+                    <img src="https://khelo.live/wp-content/uploads/2020/07/MV5BM2U5ZTIxMzUtMmQ5MC00YTdhLThjOWYtODUzNDQ0MThmMmQ4XkEyXkFqcGdeQXVyNzg5OTk2OA@@._V1_.jpg" className="max-w-xs rounded-lg shadow-2xl" />
                     <div>
                         <h1 className="text-7xl font-bold text-red-400">খেলা হবে!</h1>
                         <p className="py-6 text-xl text-black">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
