4a2e135d92e811f8bea4f64ec8ab27fe78fab05e
ShakilJoy31 <joy31a@gmail.com>
2023-05-25 15:23:22 +0600

    change

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -4,6 +4,9 @@ import connectMongo from './../../../database/connection';
 export default function handler(req, res) {
     connectMongo().catch(() => res.status(405).json({ error: 'Error while connecting.' }));
     // Type of request
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     const { method } = req;
     switch (method) {
         case 'GET':
